1a3a6a7a147916ed6303ab3dd17fb16188d02951
masoud <masoudraeeszadeh@gmail.com>
2024-02-07 23:15:26 +0330

    first

---
+++ 01.js
@@ -0,0 +1,18 @@
+console.log("masoud");
+let a=32;
+let b=4;
+console.log("32*4=",32*4);
+let m=1;
+console.log("typeof m:",typeof m ,"and this value:",m);
+m='v';
+console.log("typeof m:",typeof m ,"and this value:",m);
+m=[342,"afs",];
+console.log("typeof m:",typeof m ,"and this value:",m);
+console.log("member 0 of m:",m[0]);
+m={
+
+key:11,
+otherkey:"m"
+}
+console.log("typeof m:",typeof m ,"and this value:",m);
+console.log('m["key"] is',m["key"]);
