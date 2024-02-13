2cdfe3caa82886cf59df12c109a271a729f1550c
masoud <masoudraeeszade@gmail.com>
2024-02-13 15:56:52 +0330

    first-02

---
+++ 02/02.js
@@ -0,0 +1,15 @@
+console.log("inputs", process.argv);
+
+let inputs = [];
+
+function x (val, index)
+{
+    if(index>1)
+    {
+        inputs[index - 2] = val; }
+}
+
+process.argv.forEach(x);
+    
+console.log("input:", inputs);
+
