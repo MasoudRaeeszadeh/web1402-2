e66a01593b0c218eee5f68934587253f8ba74edb
masoud <masoudraeeszadeh@gmail.com>
2024-02-14 23:56:36 +0330

    secound-02b

--- 02/02b.js
+++ 02/02b.js
@@ -1,22 +1,24 @@
-let input = [];
-input = process.argv;
-let a = [];
-for (let i = 2; i < input.length; i+2) 
+// let input = [];
+// input = process.argv;
+// let a = [];
+// for (let i = 2; i < input.length; i+=2) 
+// {
+//     let p = {
+//         name:input[i],
+//         age:input[i+1]
+//     };
+//     a.push(p)
+
+// }
+
+// console.log(a);
+
+let inp = process.argv;
+let array = [];
+let j=0;
+for (let i = 2; i < inp.length; i+=2) 
 {
-    let p = {
-        name:input[i],
-        age:input[i+1]
-    };
-    a.push(p)
-
+    array[j]={name: inp[i],age:[i+1]};
+    j++;
 }
-
-
-console.log(a)
-
-
-
-
-
-
-
+console.log(array);
