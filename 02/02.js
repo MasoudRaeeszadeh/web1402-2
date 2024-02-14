874a107be5d8544fcd461893100e92404b48047b
masoud <masoudraeeszadeh@gmail.com>
2024-02-14 23:42:59 +0330

    secound

--- 02/02.js
+++ 02/02.js
@@ -1,15 +1,23 @@
-console.log("inputs", process.argv);
+// console.log("inputs", process.argv);
 
-let inputs = [];
+// let inputs = [];
 
-function x (val, index)
-{
-    if(index>1)
-    {
-        inputs[index - 2] = val; }
-}
+// function x (val, index)
+// {
+//     if(index>1)
+//     {
+//         inputs[index - 2] = val; }
+// }
 
-process.argv.forEach(x);
+// process.argv.forEach(x);
     
-console.log("input:", inputs);
+// console.log("input:", inputs);
+
+let len = process.argv.length;
+let array = [];
+for (let i = 2; i<len; i++) 
+{
+ array[i-2]=process.argv[i];   
+}
+console.log(array);
 
